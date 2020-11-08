9563f363032492aa4f52ecb108e97cc7378a7233
pedrocomitto <pedrcomitto@gmail.com>
2020-11-08 05:15:00 -0300

    first commit

--- src/main/kotlin/AbstractFactory.kt
+++ src/main/kotlin/AbstractFactory.kt
@@ -1,7 +1,6 @@
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import java.lang.IllegalArgumentException
 
 interface Athlete {
     val score: Int
